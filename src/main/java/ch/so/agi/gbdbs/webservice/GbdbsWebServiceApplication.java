1cb49ab155d52e3a1689872be0e0b03d6d01d55a
Stefan Ziegler <edi.gonzales@gmail.com>
2020-03-04 19:02:27 +0100

    initial commit

---
+++ src/main/java/ch/so/agi/gbdbs/webservice/GbdbsWebServiceApplication.java
@@ -0,0 +1,13 @@
+package ch.so.agi.gbdbs.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbdbsWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbdbsWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/so/agi/gbdbs/webservice/GbdbsWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.so.agi.gbdbs.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbdbsWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
