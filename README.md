69174e23da2e641dcf90c9bbf02b8d0801204162
Stefan Ziegler <edi.gonzales@gmail.com>
2020-03-06 16:37:11 +0100

    wip

--- README.md
+++ README.md
@@ -10,8 +10,11 @@ http://localhost:8080/ws/gbdbs.wsdl
 ```
 ACTION="http://schemas.geo.admin.ch/BJ/TGBV/GBDBSSvc/2.1/GetParcelsById"
 URL="http://localhost:8080/ws"
-curl -H "Content-Type: text/xml; charset=utf-8" -H "SOAPAction:$ACTION" -d@request.xml $URL
+curl -H "Content-Type: text/xml; charset=utf-8" -H "SOAPAction:$ACTION" -d@request.xml $URL > response.xml && xmllint --format response.xml
 ```
 
 ## TODO:
 - Fix schemaLocation in `http://localhost:8080/ws/gbdbs.wsdl`. Notfalls auf dem Filesystem rumschieben bis es passt.
+
+
+
--- src/main/java/ch/so/agi/gbdbs/webservice/WebServiceEndpoint.java
+++ src/main/java/ch/so/agi/gbdbs/webservice/WebServiceEndpoint.java
@@ -5,30 +5,62 @@ import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import ch.admin.geo.schemas.bj.tgbv.gbdbs._2.BezugInhalt;
 import ch.admin.geo.schemas.bj.tgbv.gbdbs._2.GetParcelsByIdRequestType;
 import ch.admin.geo.schemas.bj.tgbv.gbdbs._2.GetParcelsByIdResponse;
 import ch.admin.geo.schemas.bj.tgbv.gbdbs._2.ObjectFactory;
 
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import org.slf4j.Logger;
+
 @Endpoint
 public class WebServiceEndpoint {
+    private final Logger logger = org.slf4j.LoggerFactory.getLogger(this.getClass());
 
     private static final String NAMESPACE_URI = "http://schemas.geo.admin.ch/BJ/TGBV/GBDBS/2.1";
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "GetParcelsByIdRequest")
     @ResponsePayload
     public GetParcelsByIdResponse getParcelsById(@RequestPayload GetParcelsByIdRequestType request) {
-
-//        String outputString = "Hello " + request.getTransactionId() + "!";
-//        String outputString = "Hello " + request.getTransactionId() + "!";
-        String outputString = "Hello " + request.getIds() + "!";
-        System.out.println(outputString);
-
         ObjectFactory factory = new ObjectFactory();
-        GetParcelsByIdResponse response = factory.createGetParcelsByIdResponse();
-        System.out.println(response.toString());
+
+        // TEST
+        // Nicht klar wie das mit den Exceptions geht, eventuell: 
+        // https://docs.oracle.com/cd/E24329_01/web.1211/e24965/faults.htm#WSADV639
+        // https://docs.spring.io/spring-ws/docs/current/reference/#_code_soapfaultannotationexceptionresolver_code
+        // Steckt die GBDBS Exception im detail-Element?
+        if (!request.getBezugInhalt().equals(BezugInhalt.INDEX)) {
+            logger.info("falscher BezugInhalt");
+            ch.admin.geo.schemas.bj.tgbv.gbdbs._2.Exception exception = factory.createException();
+            exception.setMessage("falscher BezugInhalt");
+        }
         
-//        response.getGrundstuecks().add(e)
-//        response.setResult(outputString);
+        GetParcelsByIdResponse response = factory.createGetParcelsByIdResponse();
+        
+        for (String id : request.getIds()) {
+            logger.info(id.substring(0, 14));
+            
+            ch.admin.geo.schemas.bj.tgbv.gbdbs._2.GetParcelsByIdResponse.Grundstueck grundstueck = factory.createGetParcelsByIdResponseGrundstueck();
+            
+            // FIXME: use real date from database
+            try {
+                logger.info("timezone id {}", TimeZone.getDefault().getID());
+                XMLGregorianCalendar today = null;
+                GregorianCalendar gdate = new GregorianCalendar();
+                gdate.setTime(new java.util.Date());
+                today = DatatypeFactory.newInstance().newXMLGregorianCalendar(gdate);
+                response.setStandDerDaten(today);
+            } catch (DatatypeConfigurationException e) {
+                throw new IllegalStateException(e);
+            }
+            response.getGrundstuecks().add(grundstueck);    
+       }
 
         return response;
     }
