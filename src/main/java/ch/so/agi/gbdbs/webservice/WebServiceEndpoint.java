b7c532c0ad22152b19bb27ba8ce4aeafd6db309c
Stefan Ziegler <edi.gonzales@gmail.com>
2020-03-07 19:56:50 +0100

    wip

--- src/main/java/ch/so/agi/gbdbs/webservice/WebServiceEndpoint.java
+++ src/main/java/ch/so/agi/gbdbs/webservice/WebServiceEndpoint.java
@@ -99,7 +99,6 @@ public class WebServiceEndpoint {
             
             
             
-            
             JAXBElement<LiegenschaftType> jaxbLsType = new JAXBElement<LiegenschaftType>(_Liegenschaft_QNAME, LiegenschaftType.class, lsType);
             
             
